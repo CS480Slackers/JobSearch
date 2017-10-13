a04fc37c0973b034edabc8730fcb167fd6b919fd
Andrew Tek <andrewtek@gmail.com>
2017-10-12 23:01:51 -0700

    Minor routes change

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,13 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Router, Route, browserHistory} from 'react-router';
 import Homepage from '/imports/ui/pages/home/Homepage.jsx';
-import SearchPage from '/imports/ui/container/GoogleMapsContainer.jsx';
+import SearchPage from '/imports/ui/pages/home/SearchPage.jsx';
 import JobPostPage from '/imports/ui/pages/home/JobPostPage.jsx';
+import GoogleMapsContainer from '/imports/ui/container/GoogleMapsContainer.jsx';
 
 export default () =>(
   <Router history={browserHistory}>
     <Route path="/" component={Homepage} />
     <Route path="/search" component={SearchPage} />
     <Route path="/post" component={JobPostPage} />
+    <Route path="/map" component={GoogleMapsContainer} />
   </Router>
 )
