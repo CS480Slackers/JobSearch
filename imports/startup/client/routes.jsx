2b0d4e200f461e5cd1a7024d4ce682314ddb81ac
Andrew Tek <andrewtek@gmail.com>
2017-10-13 00:11:24 -0700

    Minor route change

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -9,7 +9,7 @@ import GoogleMapsContainer from '/imports/ui/container/GoogleMapsContainer.jsx';
 export default () =>(
   <Router history={browserHistory}>
     <Route path="/" component={Homepage} />
-    <Route path="/search" component={SearchPage} />
+    <Route path="/search" component={GoogleMapsContainer} />
     <Route path="/post" component={JobPostPage} />
     <Route path="/map" component={GoogleMapsContainer} />
   </Router>
