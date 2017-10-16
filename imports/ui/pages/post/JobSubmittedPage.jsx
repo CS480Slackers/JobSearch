6b6808f8d98c056882e8cad6ee9f44622c8a32d1
Eric Krapil <eric.krapil@gmail.com>
2017-10-16 14:19:10 -0700

    Create JobSubmittedPage

---
+++ imports/ui/pages/post/JobSubmittedPage.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link, browserHistory} from 'react-router';
+
+export default class JobSubmittedPage extends Component{
+  render(){
+    return(
+      <div id = "job-submitted">
+        <div id = "job-submited-header">The following job has been posted</div>
+        <div className="job-title">
+          <
+        </div>
+      </div>
+
+
+    )
+  }
+}
