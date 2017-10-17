94b5167ed4ade0d40c9f5776391032f3fea5f361
Eric Krapil <eric.krapil@gmail.com>
2017-10-17 14:40:46 -0700

    id from inserted job taken from url and stored for later use. Need to now create a container to grab the job just submitted to the database using jobId. Grabbed id from url using Route props.

--- imports/ui/pages/post/JobSubmittedPage.jsx
+++ imports/ui/pages/post/JobSubmittedPage.jsx
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import {Link, browserHistory} from 'react-router';
+//import {JobsContainer} from '/imports/ui/container/JobsContainer.jsx'
 
 export default class JobSubmittedPage extends Component{
   render(){
+    let jobId = this.props.params.jobId;
     return(
       <div id = "job-submitted">
-        <div id = "job-submited-header">The following job has been posted</div>
+        <div id = "job-submited-header">The following job has been posted with
+          ID = {jobId}</div>
         <div className="job-title">
-          <
         </div>
       </div>
-
-
     )
   }
 }
