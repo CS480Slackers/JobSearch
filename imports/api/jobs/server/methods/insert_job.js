d1a94cec3125d7143c4f5eec38bb7a482d2a55c4
Eric Krapil <eric.krapil@gmail.com>
2017-10-17 14:37:44 -0700

    jobInsert method now returns the _id of the job inserted

--- imports/api/jobs/server/methods/insert_job.js
+++ imports/api/jobs/server/methods/insert_job.js
@@ -4,6 +4,8 @@ import { Mongo } from 'meteor/mongo';
 import { Jobs } from '/imports/api/jobs/jobs.js'
 Meteor.methods({
   jobInsert:function(job){
-    Jobs.insert(job);
+    let tempId=Jobs.insert(job);
+    console.log('id', tempId);
+    return tempId;
   }
 });
