62bbd0863db5d3fef1917cd62f8f24d9537d351a
Eric Krapil <eric.krapil@gmail.com>
2017-10-17 14:36:52 -0700

    Update imports, added path to JobSubmittedPage.jsx

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -5,7 +5,8 @@ import Homepage from '/imports/ui/pages/home/Homepage.jsx';
 import SearchPage from '/imports/ui/pages/search/SearchPage.jsx';
 import JobPostPage from '/imports/ui/pages/post/JobPostPage.jsx';
 import GoogleMapsPage from '/imports/ui/component/GoogleMap.jsx';
-import JobSubmittedPage from 'imports/ui/pages/post/JobSubmittedPage.jsx';
+import JobSubmittedPage from '/imports/ui/pages/post/JobSubmittedPage.jsx';
+import JobsContainer from '/imports/ui/container/JobsContainer.jsx'
 
 export default () =>(
   <Router history={browserHistory}>
@@ -14,6 +15,6 @@ export default () =>(
     {/* <Route path="/map" component={GoogleMapsPage} /> */}
     <Route path="/post" component={JobPostPage} />
     <Route path="/map" component={GoogleMapsPage} />
-    <Route path="/submission" compontent = {JobSubmittedPage} />
+    <Route path="/submission/:jobId" component={JobSubmittedPage} />
   </Router>
 )
