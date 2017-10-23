51f10790b56dc8139bb31199564c9eb02e2cec81
Roland Lee <Roland.lee22@gmail.com>
2017-10-23 01:48:29 -0700

    commented out jobsubmit page bc it was causing errors

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -5,15 +5,15 @@ import Homepage from '/imports/ui/pages/home/Homepage.jsx';
 import SearchPage from '/imports/ui/pages/search/SearchPage.jsx';
 import JobPostPage from '/imports/ui/pages/post/JobPostPage.jsx';
 import GoogleMapsPage from '/imports/ui/component/GoogleMap.jsx';
-import JobSubmittedPage from '/imports/ui/pages/post/JobSubmittedPage.jsx';
-import JobsContainer from '/imports/ui/container/JobsContainer.jsx'
+//import JobSubmittedPage from '/imports/ui/pages/post/JobSubmittedPage.jsx';
+//import JobsContainer from '/imports/ui/container/JobsContainer.jsx'
 
 export default () =>(
   <Router history={browserHistory}>
     <Route path="/" component={Homepage} />
     <Route path="/search" component={SearchPage} />
     {/* <Route path="/map" component={GoogleMapsPage} /> */}
-    {/*<Route path="/post" component={JobPostPage} />*/}
+    <Route path="/post" component={JobPostPage} />
     <Route path="/map" component={GoogleMapsPage} />
     {/*<Route path="/submission/:jobId" component={JobSubmittedPage} />*/}
   </Router>
