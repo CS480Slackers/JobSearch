65f3d8669c98aa98dfde6bd5e68e7a892fe271e0
Krapil <eric.krapil@gmail.com>
2017-10-24 19:04:10 -0700

    update register-api

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,3 +1,6 @@
+
 import '/imports/api/jobs/jobs.js';
 import '/imports/api/jobs/server/methods/insert_job.js';
 import '/imports/api/jobs/server/methods/delete_job.js';
+import '/imports/api/jobs/classes/address.js';
+import '/imports/api/jobs/classes/job.js';
