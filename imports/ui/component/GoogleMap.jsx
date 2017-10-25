4fd1a57266e050dfe7d931ad10e63816f0beeb9e
Andrew Tek <andrewtek@gmail.com>
2017-10-24 18:27:26 -0700

    Markers are clickable and show text

--- imports/ui/component/GoogleMap.jsx
+++ imports/ui/component/GoogleMap.jsx
@@ -2,6 +2,37 @@ import React, {Component} from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class GoogleMapsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+    console.warn(this.state);
+    console.warn(this.state.selectedPlace.name);
+  }
+
+  onMapClicked = (props) => {
+    console.warn("map clicked");
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
 render() {
     let {lat, lng} = this.props;
     const style = {
@@ -16,16 +47,34 @@ render() {
     }
     return (
       <Map
-        google={this.props.google} zoom={14}
+        clickableIcons={false}
+        google={this.props.google}
         center={{lat:lat, lng:lng}}
         zoom={15}
         style={style}
+        onClick= {this.onMapClicked}
         >
         <Marker
-          // title={'The Japanese Garden.'}
-          // name={'Japanese Garden'}
+          title={'Current Location'}
+          onClick={this.onMarkerClick}
+          name={'Current Location'}
           position= {{lat:lat, lng:lng}} />
+          <Marker
+            title={'Japanese Garden'}
+            onClick={this.onMarkerClick}
+            name={'Japanese Garden'}
+            jobName={"Landscape Engineer"}
+            position= {{lat:34.0599, lng:-117.8204}} />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div style={{color: 'black'}}>
+                <h5>{this.state.selectedPlace.name}</h5>
+                <h5>{this.state.selectedPlace.jobName}</h5>
+              </div>
+            </InfoWindow>
       </Map>
+
     );
   }
 }
