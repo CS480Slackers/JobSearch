d03d1d4f9f321b352b14783d7cfe3b7ff4fdaefc
Krapil <eric.krapil@gmail.com>
2017-10-24 19:03:35 -0700

    Update JobPostPage

--- imports/ui/pages/post/JobPostPage.jsx
+++ imports/ui/pages/post/JobPostPage.jsx
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 import {Link, browserHistory} from 'react-router';
+import { Class } from 'meteor/jagi:astronomy';
+import { Job } from '/imports/api/jobs/classes/job.js';
 
 export default class JobPostPage extends Component{
   submitJob = () => {
-    let job = {
-      companyName: this.name.value,
-      location: this.location.value,
-      position: this.position.value,
-      jobDescription: this.jobDescription.value
-    }
+    var job = new Job();
+    job.company = this.name.value;
+    job.location = this.location.value;
+    job.position = this.position.value;
+    job.description = this.jobDescription.value;
+
     Meteor.call("jobInsert", job, function(error,result){
       if(result){
         let jobId = result;
         console.log("Job ID", jobId);
-        console.log("Job Posting", job);
         browserHistory.push("/submission/"+jobId);
       }
     });
