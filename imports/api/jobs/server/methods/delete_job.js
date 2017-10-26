0ba8a00e08739c77f4ddd3b895ae20e8f4b2314d
Eric Krapil <eric.krapil@gmail.com>
2017-10-26 10:40:09 -0700

    Update delete_job.js Still has to be tested

--- imports/api/jobs/server/methods/delete_job.js
+++ imports/api/jobs/server/methods/delete_job.js
@@ -3,8 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { Jobs } from '/imports/api/jobs/jobs.js'
 Meteor.methods({
-  jobRemove:function(job){
-    Jobs.remove(job);
+  jobRemove:function(id){
+    Jobs.remove({'id': id});
     return 'success';
   }
 });
